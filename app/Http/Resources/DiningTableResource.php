de26cea6bdfbc0e63d3e5d7403b200666e709e2f
Wahyu Firman <fwahyu839@gmail.com>
2026-07-29 14:55:26 +0700

    fix(dining-table): update qr url to use table route

--- app/Http/Resources/DiningTableResource.php
+++ app/Http/Resources/DiningTableResource.php
@@ -19,7 +19,7 @@ class DiningTableResource extends JsonResource
             'number' => $this->number,
             'token' => $this->token,
             'qr_url' => config('app.frontend_url')
-                . '/token/'
+                . '/table/'
                 . $this->token,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
